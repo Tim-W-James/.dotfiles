06324229c6aae2a4b93959467a80e8948621f989
Tim W James <38764646+Tim-W-James@users.noreply.github.com>
2022-03-10 14:36:50 +1100

    Added powershell modules

--- oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
 Import-Module oh-my-posh
-Import-Module -Name Terminal-Icons
 Set-PoshPrompt -Theme ~/.mytheme.omp.json
+
+Import-Module -Name Terminal-Icons
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Import-Module PSUtil
